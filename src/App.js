144a96e59b3d8d6fba38c2fc054a30ae1c4cc445
willianmenezess <willmenezes@outlook.com.br>
2023-03-13 16:00:36 -0300

    resolvido requisito 1: criadas as rotas necessárias da aplicação

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Search from './pages/Search';
+import Login from './pages/Login';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import Edit from './pages/Edit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <main>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ Edit } />
+          <Route component={ NotFound } />
+        </Switch>
+      </main>
+
+    );
   }
 }
 
---
+++ src/pages/Edit.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Edit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        <p>Edit</p>
+      </div>
+    );
+  }
+}
+
+export default Edit;
