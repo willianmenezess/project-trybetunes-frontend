8b591afe231ebc514a6f5820d1a5d6a3539696c3
willianmenezess <willmenezes@outlook.com.br>
2023-03-15 11:35:11 -0300

    resolvido requisito 8: criada uma lista de músicas favoritas e adicionando-as à função interna(api) de músicas favoritas

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,27 +1,68 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Loading from '../pages/Loading';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      favoriteSongs: [],
+      addMusic: false,
+    };
+  }
+
+  handleMusic = async (music) => {
+    // console.log(music);
+    this.setState((prevState) => ({
+      addMusic: true,
+      favoriteSongs: [...prevState.favoriteSongs, music],
+    }));
+    await addSong(music);
+    this.setState({
+      addMusic: false,
+    });
+  };
+
   render() {
     const { musics } = this.props;
+    const { addMusic, favoriteSongs } = this.state;
     return (
-
       <div>
-        {
-          musics.map((music) => (
-            <div key={ music.trackName }>
-              <p>{ music.trackName }</p>
-              <audio data-testid="audio-component" src={ music.previewUrl } controls>
-                <track kind="captions" />
-                O seu navegador não suporta o elemento
-                {' '}
-                {' '}
-                <code>audio</code>
-                .
-              </audio>
-            </div>
-          ))
-        }
+        { addMusic ? <Loading />
+          : (
+
+            <div>
+              {
+                musics.map((music) => (
+                  <div key={ music.trackName }>
+                    <p>{ music.trackName }</p>
+                    <audio
+                      data-testid="audio-component"
+                      src={ music.previewUrl }
+                      controls
+                    >
+                      <track kind="captions" />
+                      O seu navegador não suporta o elemento
+                      {' '}
+                      {' '}
+                      <code>audio</code>
+                    </audio>
+                    <label htmlFor="favorite">
+                      Favorita
+                      <input
+                        data-testid={ `checkbox-music-${music.trackId}` }
+                        id="favorite"
+                        type="checkbox"
+                        name="favorite"
+                        checked={ favoriteSongs.some((item) => item === music) }
+                        onChange={ () => this.handleMusic(music) }
+                      />
+                    </label>
+                  </div>
+                ))
+              }
+            </div>)}
       </div>
     );
   }
