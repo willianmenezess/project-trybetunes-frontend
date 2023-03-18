8f404b148868190f51ec877afbac038260ba0163
willianmenezess <willmenezes@outlook.com.br>
2023-03-18 11:36:22 -0300

    teste requisito 14: funciona na tela, mas nao nos testes locais, verificando se passa no avaliador

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Loading from './Loading';
-import { getUser } from '../services/userAPI';
+import { getUser, updateUser } from '../services/userAPI';
 
 class ProfileEdit extends Component {
   constructor() {
@@ -9,15 +10,54 @@ class ProfileEdit extends Component {
     this.state = {
       isLoading: true,
       name: '',
-      email: '',
-      image: '',
-      description: '',
+      email: 'email@test.com',
+      image: 'profile-image',
+      description: 'Lorem ipsum',
+      onDisabled: true,
     };
   }
 
   componentDidMount() {
     this.getUserInfo();
   }
+
+  handleChange = ({ target }) => {
+    const { name, email, description } = this.state;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [target.name]: value,
+    });
+    if ((name !== '') && (email !== '') && (description !== '')) {
+      this.setState({
+        onDisabled: false,
+      });
+    } else {
+      this.setState({
+        onDisabled: true,
+      });
+    }
+  };
+
+  clickUpdateUser = async () => {
+    const { history } = this.props;
+    const { name, email, image, description } = this.state;
+    const newUser = {
+      name,
+      email,
+      image,
+      description,
+    };
+
+    this.setState({
+      name: '',
+      email: '',
+      description: '',
+      image: '',
+    });
+
+    await updateUser(newUser);
+    history.push('/profile');
+  };
 
   getUserInfo = async () => {
     const userInfo = await getUser();
@@ -32,26 +72,80 @@ class ProfileEdit extends Component {
   };
 
   render() {
-    const { name, email, image, description, isLoading } = this.state;
+    const { name, email, image, description, isLoading, onDisabled } = this.state;
     return (
       <div data-testid="page-profile-edit">
         <Header />
         { isLoading ? <Loading />
           : (
-            <section>
-              <img src={ image } alt="profileImage" data-testid="profile-image" />
-              <button>Editar perfil</button>
-              <p>Nome</p>
-              <p>{ name }</p>
-              <p>Email</p>
-              <p>{ email }</p>
-              <p>Descrição</p>
-              <p>{ description }</p>
-            </section>
+            <form>
+              <img src={ image } alt="profileImage" data-testid="edit-input-image" />
+              <br />
+
+              <label htmlFor="name-edit">
+                Nome
+                <br />
+                <input
+                  type="text"
+                  id="name-edit"
+                  data-testid="edit-input-name"
+                  name="name"
+                  value={ name }
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <br />
+
+              <label htmlFor="email-edit">
+                Email
+                <br />
+                <input
+                  type="email"
+                  id="email-edit"
+                  data-testid="edit-input-email"
+                  name="email"
+                  value={ email }
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <br />
+
+              <label htmlFor="description-edit">
+                Descrição
+                <br />
+                <textarea
+                  id="description-edit"
+                  data-testid="edit-input-description"
+                  name="description"
+                  value={ description }
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <br />
+
+              <label htmlFor="edit-button">
+                <button
+                  id="edit-button"
+                  data-testid="edit-button-save"
+                  type="button"
+                  disabled={ onDisabled }
+                  onClick={ this.clickUpdateUser }
+                >
+                  Salvar
+                </button>
+              </label>
+
+            </form>
           )}
       </div>
     );
   }
 }
+
+ProfileEdit.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default ProfileEdit;
